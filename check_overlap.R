2537d0f7f7a0797678aa659ae47e1f79d89890d2
KateMMiller <kathryn_miller@nps.gov>
2021-01-18 10:53:45 -0500

    added check of plots covering other plots

--- check_overlap.R
+++ check_overlap.R
@@ -1,6 +1,7 @@
 #----------------------
 # Function to eventually add to forestNETN/MIDN
 # Internal function used within nudge_XY
+#   - There's still a check of overlap I'm missing- if a buffer is inside a plot only the inside plot is caught
 #----------------------
 # ID plots that have at least one overlapping pie. Used within nudge_XY
 check_overlap <- function(sf){
@@ -19,8 +20,13 @@ check_overlap <- function(sf){
     bind_rows() %>% mutate(Plot_Name = plot_list) %>% 
     filter(inside > 0) %>% select(Plot_Name) %>% mutate(type = 'within')
   
+  covers <- st_covers(sf_buff) %>% set_names(plot_list) %>% 
+    lapply(FUN = function(x) data.frame(covers = length(x)-1)) %>% 
+    bind_rows() %>% mutate(Plot_Name = plot_list) %>% 
+    filter(covers > 0) %>% select(Plot_Name) %>% mutate(type = 'covers')
+  
   # Bind plot list of pies to shift coords
-  plots_to_shift <- rbind(overlaps, withins) %>% arrange(Plot_Name, type) %>% 
+  plots_to_shift <- rbind(overlaps, withins, covers) %>% arrange(Plot_Name, type) %>% 
     select(Plot_Name) %>% unique()
   
   return(plots_to_shift)
