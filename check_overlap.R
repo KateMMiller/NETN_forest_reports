a171b4b3cc76a99883288e8ae9ffdf1af670bf1c
KateMMiller <kathryn_miller@nps.gov>
2021-01-18 13:55:04 -0500

    added more overlap checks

--- check_overlap.R
+++ check_overlap.R
@@ -9,10 +9,15 @@ check_overlap <- function(sf){
   sf_buff <- st_buffer(sf, sf$fig_radius)
   
   # Determine pies that overlap
-  overlaps <- st_drop_geometry(st_intersection(sf_buff)) %>% group_by(Plot_Name) %>% 
-    summarize(num_overlaps = sum(!is.na(Plot_Name))-1, 
-              .groups = "drop") %>% ungroup() %>% 
-    filter(num_overlaps > 0) %>% select(Plot_Name) %>% mutate(type = 'overlap')
+  overlaps1 <- st_drop_geometry(st_intersection(sf_buff)) %>% group_by(Plot_Name) %>%
+    summarize(num_overlaps = sum(!is.na(Plot_Name))-1,
+              .groups = "drop") %>% ungroup() %>%
+    filter(num_overlaps > 0) %>% select(Plot_Name) %>% mutate(type = 'overlap1')
+
+  overlaps2 <- st_overlaps(sf_buff) %>% set_names(plot_list) %>% 
+    lapply(FUN = function(x) data.frame(overs = length(x))) %>% 
+    bind_rows() %>% mutate(Plot_Name = plot_list) %>% 
+    filter(overs > 0) %>% select(Plot_Name) %>% mutate(type = 'overlap2')
   
   # Determine pies that are within another pie
   withins <- st_within(sf_buff) %>% set_names(plot_list) %>% 
@@ -26,7 +31,7 @@ check_overlap <- function(sf){
     filter(covers > 0) %>% select(Plot_Name) %>% mutate(type = 'covers')
   
   # Bind plot list of pies to shift coords
-  plots_to_shift <- rbind(overlaps, withins, covers) %>% arrange(Plot_Name, type) %>% 
+  plots_to_shift <- rbind(overlaps1, overlaps2, withins, covers) %>% arrange(Plot_Name, type) %>% 
     select(Plot_Name) %>% unique()
   
   return(plots_to_shift)
