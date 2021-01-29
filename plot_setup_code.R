789c0b8ce6640f9dffdfd04bc2281f77c776ed6b
KateMMiller <kathryn_miller@nps.gov>
2021-01-29 15:54:10 -0500

    added veg_leg

--- plot_setup_code.R
+++ plot_setup_code.R
@@ -103,4 +103,18 @@ pie_fun <- function(df, plotname, y_var, grp_var, std_var){
                  height = unit(pie_exp, "cm"))
   }
 
+#----- Create veg map legend -----
+veg_leg <- tm_shape(park_veg)+
+  tm_fill(col = "fills")+
+  tm_add_legend(title = "Habitat types", type = 'fill', 
+                labels = park_veg$labels,
+                col = park_veg$fills,
+                border.col = "white",
+                border.lwd = 3,
+                size = 0.5)+
+  tm_layout(legend.only = TRUE, 
+            legend.text.size = 1,
+            legend.title.size = 1.2)
+
+
 
